5c8e3337e4be182c1c827c35951b7748ce15b019
Aleksandar Ristic <aristic@software-nation.com>
2018-01-24 16:35:36 +0100

    Transfer Photo.js,PhotoWall.js and Title.js to functional component

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,12 +1,11 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Photo from './Photo'
 
-class PhotoWall extends Component {
-    render() {
-        return <div className="photo-grid">
-                  {this.props.posts.map((post, index) => <Photo key={index} post={post}/>)}
-               </div>
-    }
-}
+
+function PhotoWall(props) {
+    return <div className="photo-grid">
+            {props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+           </div>
+};
 
 export default PhotoWall
