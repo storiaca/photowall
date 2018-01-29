e69ab71e6cd77a79bb0eb5f66eb8fcb873c3c3fa
Aleksandar Ristic <storiaca@gmail.com>
2018-01-29 15:26:24 +0100

    Add redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import Main from './Components/Main'
 import './styles/stylesheet.css'
 import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+
+const store = createStore()
 
 ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>, document.getElementById('root'));
 
