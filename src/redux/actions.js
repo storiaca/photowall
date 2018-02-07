3605a455c366626ab97e32b61119fb473f14ee12
Aleksandar Ristic <storiaca@gmail.com>
2018-02-07 16:24:42 +0100

    Selectively loading comments

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -18,9 +18,10 @@ export function addPost(post) {
 
 // adding comments action
 
-export function addComment(comment) {
+export function addComment(comment, postId) {
     return {
         type: 'ADD_COMMENT',
-        comment
+        comment, 
+        postId
     }
 }
